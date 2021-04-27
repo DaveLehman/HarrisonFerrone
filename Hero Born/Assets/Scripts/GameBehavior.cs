a3a86c9f66da3f00c7aae4f0b60c7b5649477081
DaveLehman <lemo@provide.net>
2021-04-27 06:46:27 -0400

    Ch08 working pre-UI

---
+++ Hero Born/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour
+{
+    private int _items_collected = 0;
+    private int _playerHP = 10;
+    public int Items
+    {
+        get { return _items_collected; }
+        set
+        {
+            _items_collected = value;
+            Debug.LogFormat("Items: {0}", _items_collected);
+        }
+    }
+    public int PlayerHP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hero Born/Assets/Scripts/PickupItemBehavior.cs
+++ Hero Born/Assets/Scripts/PickupItemBehavior.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PickupItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+        if (gameManager == null)
+        {
+            Debug.Log("PickupItem script failed to find a game manager!");
+        }
     }
 
     // Update is called once per frame
@@ -18,11 +24,12 @@ public class PickupItemBehavior : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("A collision occurred");
+        //Debug.Log("A collision occurred");
         if (collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+            gameManager.Items += 1;
         }
     }
 }
